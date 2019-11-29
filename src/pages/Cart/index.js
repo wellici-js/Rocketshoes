3421145659a26be8167a6db144527b002b7c19d8
Wellici Araujo <wja1@ifal.edu.br>
2019-11-29 10:01:17 -0300

    connected with api

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,9 +1,64 @@
 import React from 'react';
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete
+} from 'react-icons/md';
 
-// import { Container } from './styles';
+import { Container, ProductTable, Total } from './styles';
 
 export default function Cart() {
   return (
-    <h1>Cart</h1>
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>PRODUTO</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img src="https://static.netshoes.com.br/produtos/bota-coturno-sapatofran-ziper-masculino/06/HAP-0022-006/HAP-0022-006_detalhe2.jpg?ims=326x"
+                alt="bota masculina" />
+            </td>
+            <td>
+              <strong>Bota maculina</strong>
+              <span>R$129,90</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input type="number" readOnly value={1} />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>R$258,80</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="#7159c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+      <footer>
+        <button type="button">finalizar pedido</button>
+        <Total>
+          <span>TOTAL</span>
+          <strong>R$1899,28</strong>
+        </Total>
+      </footer>
+    </Container>
   );
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,76 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
+
+import api from '../../services/api';
 
 import { ProductList } from './styles';
 
-export default function Home() {
-  return (
-    <ProductList>
-      <li>
-        <img src="https://static.netshoes.com.br/produtos/bota-coturno-sapatofran-ziper-masculino/06/HAP-0022-006/HAP-0022-006_detalhe2.jpg?ims=326x"
-          alt="Bota masculina" />
-        <strong>Bota masculina</strong>
-        <span>R$129,90</span>
+export default class Home extends Component {
+  state = {
+    products: []
+  }
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" />
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img src="https://static.netshoes.com.br/produtos/bota-coturno-sapatofran-ziper-masculino/06/HAP-0022-006/HAP-0022-006_detalhe2.jpg?ims=326x"
-          alt="Bota masculina" />
-        <strong>Bota masculina</strong>
-        <span>R$129,90</span>
+  async componentDidMount() {
+    const response = await api.get('products');
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" />
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img src="https://static.netshoes.com.br/produtos/bota-coturno-sapatofran-ziper-masculino/06/HAP-0022-006/HAP-0022-006_detalhe2.jpg?ims=326x"
-          alt="Bota masculina" />
-        <strong>Bota masculina</strong>
-        <span>R$129,90</span>
+    this.setState({ products: response.data });
+  }
+  render() {
+    const { products } = this.state;
+    return (
+      <ProductList>
+        {products.map(product => (
+          <li key={product.id}>
+            <img src={product.image}
+              alt={product.title} />
+            <strong>Bota masculina</strong>
+            <span>R$129,90</span>
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" />
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img src="https://static.netshoes.com.br/produtos/bota-coturno-sapatofran-ziper-masculino/06/HAP-0022-006/HAP-0022-006_detalhe2.jpg?ims=326x"
-          alt="Bota masculina" />
-        <strong>Bota masculina</strong>
-        <span>R$129,90</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" />
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img src="https://static.netshoes.com.br/produtos/bota-coturno-sapatofran-ziper-masculino/06/HAP-0022-006/HAP-0022-006_detalhe2.jpg?ims=326x"
-          alt="Bota masculina" />
-        <strong>Bota masculina</strong>
-        <span>R$129,90</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" />
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-    </ProductList>
-  );
+            <button type="button">
+              <div>
+                <MdAddShoppingCart size={16} color="#fff" />
+              </div>
+              <span>ADICIONAR AO CARRINHO</span>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+    );
+  }
 }
